909e564f2a6e1aa02e43c1cf6f30efc2262a562d
Mikhail <mixailshurov@gmail.com>
2022-12-09 00:17:36 +0300

    , added

--- Parsers.py
+++ Parsers.py
@@ -82,7 +82,7 @@ def parsers():
                         headers=headers, timeout=(10, 20))
                     soup = BeautifulSoup(wise_val.text, 'lxml')
                     price = soup.find("span", "text-success").text
-                    price = price[:price.find('.')] + price[price.find('.') + 1:]
+                    price = price[:price.find('.')] + "," + price[price.find('.') + 1:]
                     wise.append([round(float(price), 3)])
                 except:  # NOQA
                     wise.append([""])
