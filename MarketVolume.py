337417af383e74de8a0a829d0ab2ee08866f98d3
Mikhail <mixailshurov@gmail.com>
2023-01-03 18:57:03 +0300

    test

--- MarketVolume.py
+++ MarketVolume.py
@@ -99,7 +99,7 @@ def collect_v():
                 visa_response = requests.get(
                     f"https://cis.visa.com/cmsapi/fx/rates?amount=1&fee=0&utcConvertedDate={str_current_date}&exchangedate={str_current_date}&fromCurr={fiats[fiat]}&toCurr=USD",
                     headers=headers).text
-                print(visa_response)
+                print(str_current_date, visa_response, fiats[fiat])
                 visa_response = json.loads(visa_response)
                 tmp = visa_response["convertedAmount"]
                 tmp = tmp.replace(',', '')
